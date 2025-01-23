9cd8fb7750c3c8292f734a57bfdf0450595bb094
chabikant <chabikant@gmail.com>
2025-01-23 12:33:41 +0530

    update

--- src/Componenets/Products.js
+++ src/Componenets/Products.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { CiHeart } from "react-icons/ci";
-import './Products.css';  
+import './Products.css';
 
 const Products = ({ img, title }) => {
   return (
-    <div className='card'>
-      <img src={img} alt="Product" className='card-img' />
-      <div className='card-details'>
-        <h3 className='card-title'>{title}</h3>
-        <h6 className='login'>Sign in or <br></br>create an <br></br>account to <br></br>see pricing<br></br> <CiHeart/></h6>
+    <div className="card">
+      <div className="card-img-container">
+        <img src={img} alt={title} className="card-img" />
+      </div>
+      <div className="card-details">
+        <h3 className="card-title">{title}</h3>
+        <p className="card-description">
+          Sign in or create an account to see pricing
+        </p>
+        <CiHeart className="heart-icon" />
       </div>
     </div>
   );
